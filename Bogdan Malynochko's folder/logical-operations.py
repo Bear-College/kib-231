a02087280969b377f002f0a75f7546ae2a122a2d
BogdanMalynochko <bogdanmalynochko@gmail.com>
2023-09-14 17:33:26 +0300

    logical operations

---
+++ Bogdan Malynochko's folder/logical-operations.py
@@ -0,0 +1,24 @@
+# 1)
+a = 15;
+b = 16;
+
+result = a == 15 and b == 16;
+
+print(result);
+
+# 2)
+c = 2398;
+d = 1239236;
+
+result = c < 15 and d == 16;
+
+print(result);
+
+# 3)
+married = False;
+print(married == False or 15 == 15);
+
+# 4
+
+number = 1000;
+print(not number > 100);
