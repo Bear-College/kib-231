9e04cf0cf18acc10bcb84794533553a31bc63616
Maks <guser3025@gmail.com>
2023-09-15 15:05:34 +0300

    Done

---
+++ Maksym Shek's folder/lab2/coment.py
@@ -0,0 +1,48 @@
+print ("Hello World")
+
+userName = "Maks"
+
+user_name = "Maks"
+
+name = "Maks"
+
+isMaried = False
+print(isMaried)
+
+isAlive = True
+print(isAlive)
+
+age = 18
+print("Вік:", age) #Вік: 18
+
+count = 9
+print("Кількість:", count) #кількість:9
+
+height = 1.68
+pi = 3.14
+weight = 68
+print(height) # 1.68
+print(pi) # 3.14
+print(weight) # 68
+
+# str
+message = "Hello World"
+print(message) #Hello World
+
+name = "Maks"
+print(name) # Maks
+
+#type()
+userId = "abc" # тип str
+print(type(userId)) # <class 'str'>
+
+userId = 234 # тип int
+print(type(userId)) # <class 'int'>
+
+print("Hello World")
+print("Hello METANIT.COM")
+print("Hello Python")
+
+name = input("Your name :")
+ade = input("Your age :")
+print("Name:{name} Age:{age}")
