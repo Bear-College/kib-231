d32131529e1e722305276335f91e9277cc59c893
DIMA SHCHENSNEVYCH <shensnevich.dima@gmail.com>
2023-09-15 14:19:14 +0300

    move to lab2 folder

---
+++ Dima Shchensnevych's folder/lab2/Calculator.py
@@ -0,0 +1,20 @@
+op=input("Яку операію слід виконати +, -, * або / :")
+
+a=float(input("Введіт перше число :"))
+
+b=float(input("Введіт друге число :"))
+
+if op == "+":
+    c=a+b
+    print(c)
+elif op == "-":
+    c=a-b
+    print(c)
+elif op == "*":
+    c=a*b
+    print(c)
+elif op == "/":
+    c=a/b
+    print(c)
+else:
+    print("Введено невірну операцію!")
