bdee001391c9be67490630230aa211ebe19368f4
DIMA SHCHENSNEVYCH <shensnevich.dima@gmail.com>
2023-09-15 14:18:08 +0300

    move to lab2 folder

---
+++ Dima Shchensnevych's folder/lab2/vars.py
@@ -0,0 +1,50 @@
+username="Dima"
+
+user_name="Dima"
+
+name="Dima"
+
+#Логічний тип даних bool
+isMarried=False
+print(isMarried)
+
+isAlive= True
+print(isAlive)
+
+#Цілий тип даних int
+
+age= 19
+
+print("Вік:",age)
+
+count=13
+
+print("Кількість:",count)
+
+#Дробовий тип даних float
+
+height=1.76
+pi=3.14
+weight=89.3
+
+print(height)
+print(pi)
+print(weight)
+
+#Стрічковий тип даних
+
+message="hello world"
+
+print(message)
+
+name="Dima"
+
+print(name)
+
+#Функція type()
+
+userid1='abc'
+print(type(userid1))
+
+userid2=234
+print(type(userid2))
