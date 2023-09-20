537ba1744bd5965353cfd4eabd2adb563beef206
wertolet2 <testilka91@gmail.com>
2023-09-20 14:39:07 +0300

    arithmetic op / cond con / cond con / vars / input

---
+++ Vadym Ivchenko/lab2/conditional operations.py
@@ -0,0 +1,38 @@
+a = 4
+b = 2
+
+# a дорівнює b
+if a == b:
+    print(True)
+else:
+    print(False)
+
+# a не дорівнює b
+if a != b:
+    print(True)
+else:
+    print(False)
+
+# a більше b
+if a > b:
+    print(True)
+else:
+    print(False)
+
+# a менше b
+if a < b:
+    print(True)
+else:
+    print(False)
+
+# a більше або дорівнює b
+if a >= b:
+    print(True)
+else:
+    print(False)
+
+# a менше фбо дорівнює b
+if a <= b:
+    print(True)
+else:
+    print(False)
---
+++ Vadym Ivchenko/lab2/vars.py
@@ -0,0 +1,41 @@
+userName = "Vadym"
+
+user_name = "Vadym"
+
+name = "Vadym"
+
+# bool
+isMarried = False
+print(isMarried)  # False
+
+isAlive = True
+print(isAlive)    # True
+
+# int
+age = 18
+print("Вік:", age)     # Вік: 18
+
+count = 54 
+print("Кількість:", count)   # Кількість: 15
+
+# float
+height = 54.54
+pi = 3.14
+weight = 54
+print(height)  # 54.54
+print(pi)   # 3.14
+print(weight)  # 54
+
+# str
+message = "I need more bullets"
+print(message)  # I need more bullets
+
+name = 'Vadym'
+print(name)  # Vadym
+
+# type()
+userId1 = "abc"
+print(type(userId1)) 
+
+userId2 = 234
+print(type(userId2))
