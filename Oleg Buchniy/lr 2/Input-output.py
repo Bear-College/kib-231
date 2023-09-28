ac2d1272753607e4b988af80871843c306b992a5
OlegBuchniy <mr.bychnuy@gmail.com>
2023-09-28 12:59:48 +0300

    input-output

---
+++ Oleg Buchniy/lr 2/Input-output.py
@@ -0,0 +1,4 @@
+print ("Hello I'm Gladiator 1 \n What is your name?")
+NamrGladiator2 = input("Hello my name is... ")
+print(f"Nice to meet you {NamrGladiator2} \n How old are you?")
+print("I'm ..")
