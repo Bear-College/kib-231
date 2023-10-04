6afb881058c2735059394be8876daf904a7a9236
DIMA SHCHENSNEVYCH <shensnevich.dima@gmail.com>
2023-10-04 17:48:17 +0300

    updated wrfiles

--- Dima Shchensnevych's folder/lab5-6/wrfiles.py
+++ Dima Shchensnevych's folder/lab5-6/wrfiles.py
@@ -1,9 +1,26 @@
-text = input("Print some text: ")
+import csv
+text= input("Введіть текст: ")
+
 with open("C:\DIMA SHCHENSNEVYCH\PythonBachelor\Dima Shchensnevych's folder\lab5-6\myfile.txt", "w") as file:
         file.write (text)
 
 with open("C:\DIMA SHCHENSNEVYCH\PythonBachelor\Dima Shchensnevych's folder\lab5-6\myfile.txt", "r") as file:
         conten = file.read()
         print(conten)
+
+
+name, age = input("Введіть дані користувача: ").split()      
+user = [
+        [name, age]
+] 
+
+with open("C:\DIMA SHCHENSNEVYCH\PythonBachelor\Dima Shchensnevych's folder\lab5-6\myfile2.csv", "w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerows(user)
+
+with open("C:\DIMA SHCHENSNEVYCH\PythonBachelor\Dima Shchensnevych's folder\lab5-6\myfile2.csv", "r", newline="") as file:
+        reader = csv.reader(file)
+        for row in reader:
+                print(row[0], " - " ,row[1])
 
 
