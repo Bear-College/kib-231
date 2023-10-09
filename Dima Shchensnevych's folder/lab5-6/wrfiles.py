7d2e5756f5f7e39a83ede6641c5314ba6afd7224
DIMA SHCHENSNEVYCH <shensnevich.dima@gmail.com>
2023-10-09 12:25:00 +0300

    finished wrfiles

--- Dima Shchensnevych's folder/lab5-6/wrfiles.py
+++ Dima Shchensnevych's folder/lab5-6/wrfiles.py
@@ -1,12 +1,14 @@
 import csv
+import pickle
+
 text= input("Введіть текст: ")
 
 with open("C:\DIMA SHCHENSNEVYCH\PythonBachelor\Dima Shchensnevych's folder\lab5-6\myfile.txt", "w") as file:
         file.write (text)
 
 with open("C:\DIMA SHCHENSNEVYCH\PythonBachelor\Dima Shchensnevych's folder\lab5-6\myfile.txt", "r") as file:
-        conten = file.read()
-        print(conten)
+        content = file.read()
+        print(content)
 
 
 name, age = input("Введіть дані користувача: ").split()      
@@ -21,6 +23,13 @@ with open("C:\DIMA SHCHENSNEVYCH\PythonBachelor\Dima Shchensnevych's folder\lab5
 with open("C:\DIMA SHCHENSNEVYCH\PythonBachelor\Dima Shchensnevych's folder\lab5-6\myfile2.csv", "r", newline="") as file:
         reader = csv.reader(file)
         for row in reader:
-                print(row[0], " - " ,row[1])
+                print("Ім'я:",row[0],"\tВік:",row[1])
 
+with open("C:\DIMA SHCHENSNEVYCH\PythonBachelor\Dima Shchensnevych's folder\lab5-6\myfile3.dat", "wb") as file:
+        pickle.dump(user, file)
+
+with open("C:\DIMA SHCHENSNEVYCH\PythonBachelor\Dima Shchensnevych's folder\lab5-6\myfile3.dat", "rb") as file:
+        content2 = pickle.load(file)
+        for user in content2:
+                print("Ім'я:", user[0], "\tВік:", user[1])
 
