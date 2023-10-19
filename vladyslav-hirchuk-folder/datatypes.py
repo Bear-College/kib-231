8c48dbd44c3f2b7d221a1b8bcae0016092743695
Vladyslav Hirchuk <145594568+SoloScriptSage@users.noreply.github.com>
2023-10-19 02:32:39 -0500

    Create datatypes.py

---
+++ vladyslav-hirchuk-folder/datatypes.py
@@ -0,0 +1,10 @@
+
+#variables-and-datatypes
+
+name = "Vladyslav Hirchuk" #string
+age = 19 #int
+my_weight = 60.1 #float
+bool_example = True
+
+
+print(f"Hello, my name is {name}, and I'm {age}. My weight is {my_weight} and it's {bool_example} that I love coding!")
