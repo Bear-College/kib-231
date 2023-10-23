5f411b9e976151c63e5d6a0c27eaa679dc57f44a
Vladyslav Hirchuk <145594568+SoloScriptSage@users.noreply.github.com>
2023-10-22 22:41:33 -0500

    Create conversion.py

---
+++ vladyslav-hirchuk-folder/lab8/conversion.py
@@ -0,0 +1,16 @@
+
+a = float(input("Input a first number: "))
+b = float(input("Input a second number: "))
+
+int_sum = int(a) + int(b)
+float_sum = float(a) + int(b)
+string_sum = str(a) + str(b)
+bool_sum = bool(a) != bool(b)
+
+print(int_sum)
+print(float_sum)
+print(string_sum)
+print(bool_sum)
+
+
+
