ea21df2d5d416b68009e2f4611d7dd40d553f7c4
Yaroslav <biomerty@gmail.com>
2023-10-24 17:42:50 +0300

    Finished

---
+++ Yaroslav Hurskyi folder/lab2/vars.py
@@ -0,0 +1,42 @@
+
+username="Yaroslav"
+
+user_name="Yaroslav"
+
+name="Yaroslav"
+
+#Логічний тип даних bool
+isMarried = False
+print(isMarried)
+
+isAlive = True
+print(isAlive)
+
+
+#Дробовий тип даних float
+
+height=1.78
+pi=3.14
+weight=54.4
+
+print(height)
+print(pi)
+print(weight)
+
+#Стрічковий тип даних
+
+message="ultra hi world"
+
+print(message)
+
+name="Yaroslav"
+
+print(name)
+
+#Функція type()
+
+userid1='abc'
+print(type(userid1))
+
+userid2=54
+print(type(userid2))
